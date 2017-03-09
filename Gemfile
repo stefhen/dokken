5c2319fcac4f71aea91bc576415019594ea718ef
Stefhen Hovland <stefhen@rightscale.com>
2017-03-09 13:25:42 -0500

    stuffz

--- Gemfile
+++ Gemfile
@@ -1,6 +1,1 @@
 source 'https://rubygems.org'
-
-#gem 'chef', '~> 12.16.0'
-#gem 'berkshelf'
-#gem 'kitchen-dokken'
-#gem 'kitchen-vagrant'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,8 @@
 node['dokken']['packages'].each do |p|
   package p
 end
+
+if node[:platform] == 'ubuntu'
+  include_recipe 'apt'
+  include_recipe 'java'
+end
