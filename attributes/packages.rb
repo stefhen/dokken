d7df073e190a7317be38dbcf83a16bb31b4acfd2
Stefhen Hovland <stefhen@rightscale.com>
2017-07-18 10:55:07 -0700

    Use centos

--- attributes/packages.rb
+++ attributes/packages.rb
@@ -2,6 +2,6 @@ if node[:platform] == 'ubuntu'
   default['dokken']['packages'] = %w[nmap rsync screen lftp netcat]
 end
 
-if node[:platform] == 'redhat'
+if node[:platform] == 'centos'
   default['dokken']['packages'] = %w[nmap rsync screen lftp nc]
 end
